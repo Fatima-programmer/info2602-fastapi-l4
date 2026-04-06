728d0d0fe447f11338e6cef5840e5ac7a97c8620
Fatima-programmer <mohammedfmfatima@gmail.com>
2026-04-06 11:23:35 -0400

    Update main.py

--- app/main.py
+++ app/main.py
@@ -5,5 +5,14 @@ from app.routers import main_router
 app = FastAPI()
 app.include_router(main_router)
 
+# Add this section of code,
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 if __name__ == "__main__":
     uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
